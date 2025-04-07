48a5be1311ba37b7b4164b6ce1c744f555c8044a
Omenaga <justin.wu000@gmail.com>
2025-04-06 20:23:51 -0400

    Part 2?

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,14 @@ public class GameManager : MonoBehaviour
 
     public GameObject enemyOnePrefab;
     public GameObject enemyTwoPrefab;
+    public GameObject coinPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1, 2);
         InvokeRepeating("CreateEnemyTwo", 2, 4);
+        InvokeRepeating("CreateCoin", 5, 10);
     }
 
     // Update is called once per frame
@@ -34,6 +36,11 @@ public class GameManager : MonoBehaviour
         // Prevent off-screen zigzag
         float spawnX = Random.Range(-9f + magnitude, 9f - magnitude);  // Adjusted spawn range
         Instantiate(enemyTwoPrefab, new Vector3(spawnX, 6.5f, 0), Quaternion.identity);
+    }
+
+    void CreateCoin()
+    {
+        Instantiate(coinPrefab, new Vector3(-9.5f, Random.Range(-3.5f, 1f), 0), Quaternion.identity);
     }
 
     
